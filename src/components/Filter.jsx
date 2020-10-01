7e45c938b794d6b09e452f21ce1046109446d7f3
Vlad Kuzmin <devlad@bk.ru>
2020-10-01 14:37:14 +0200

    added_search

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Input, Menu } from 'semantic-ui-react'
 
-const Filter = ({ setFilter, filterBy }) => (
+const Filter = ({ setFilter, filterBy, searchQuery, setSearchQuery }) => (
     <Menu secondary>
           <Menu.Item
             active={filterBy === 'all'}
@@ -19,6 +19,12 @@ const Filter = ({ setFilter, filterBy }) => (
             active={filterBy === 'author'}
             onClick={setFilter.bind(this, 'author')}
           >Автор</Menu.Item>
+          <Menu.Item
+          ><Input icon="search" 
+                  onChange={ e => setSearchQuery(e.target.value) }
+                  value={ searchQuery } 
+                  placeholder="Введите запрос..." />
+            </Menu.Item>
         </Menu>
 );
 
